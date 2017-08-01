51c44c5a479a636e8f0a4401043c6f7c5401f47c
Hasan Zamil Ahmed <hzamil17@gmail.com>
2017-08-01 18:56:13 +0600

    Modified

--- contact.php
+++ contact.php
@@ -2,8 +2,8 @@
 
  <div class="main">
     <div class="content">
-    	<div class="support">
-  			<div class="support_desc">
+    	<div class="support">   
+  			  <div class="support_desc">
   				<h3>Live Support</h3>
   				<p><span>24 hours | 7 days a week | 365 days a year &nbsp;&nbsp; Live Technical Support</span></p>
   				<p> It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.</p>
