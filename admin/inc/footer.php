98e0e1465432539749291e0d39800c6feabd58e2
ornab <hzamil17@gmail.com>
2017-08-01 18:44:22 +0600

    Modified

---
+++ admin/inc/footer.php
@@ -0,0 +1,12 @@
+ <div class="clear">
+        </div>
+    </div>
+    <div class="clear">
+    </div>
+    <div id="site_info">
+        <p>
+         &copy; Copyright <a href="http://musical instrument.com"> </a>Touhid @cste_7th
+        </p>
+    </div>
+</body>
+</html>
---
+++ admin/inc/header.php
@@ -0,0 +1,89 @@
+<?php
+ include '../lib/Session.php';
+        Session::checkSession();
+?>
+
+<?php
+  header("Cache-Control: no-cache, must-revalidate");
+  header("Pragma: no-cache"); 
+  header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); 
+  header("Cache-Control: max-age=2592000");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <title>Admin Panel</title>
+    <link rel="stylesheet" type="text/css" href="css/reset.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="css/text.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="css/grid.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="css/layout.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="css/nav.css" media="screen" />
+    <link href="css/table/demo_page.css" rel="stylesheet" type="text/css" />
+    <!-- BEGIN: load jquery -->
+    <script src="js/jquery-1.6.4.min.js" type="text/javascript"></script>
+    <script type="text/javascript" src="js/jquery-ui/jquery.ui.core.min.js"></script>
+    <script src="js/jquery-ui/jquery.ui.widget.min.js" type="text/javascript"></script>
+    <script src="js/jquery-ui/jquery.ui.accordion.min.js" type="text/javascript"></script>
+    <script src="js/jquery-ui/jquery.effects.core.min.js" type="text/javascript"></script>
+    <script src="js/jquery-ui/jquery.effects.slide.min.js" type="text/javascript"></script>
+    <script src="js/jquery-ui/jquery.ui.mouse.min.js" type="text/javascript"></script>
+    <script src="js/jquery-ui/jquery.ui.sortable.min.js" type="text/javascript"></script>
+    <script src="js/table/jquery.dataTables.min.js" type="text/javascript"></script>
+    <!-- END: load jquery -->
+    <script type="text/javascript" src="js/table/table.js"></script>
+    <script src="js/setup.js" type="text/javascript"></script>
+	 <script type="text/javascript">
+        $(document).ready(function () {
+            setupLeftMenu();
+		    setSidebarHeight();
+        });
+    </script>
+
+</head>
+<body>
+    <div class="container_12">
+        <div class="grid_12 header-repeat">
+            <div id="branding">
+                <div class="floatleft logo">
+                    <img src="img/logoth.png" alt="Logo" />
+				</div>
+				<div class="floatleft middle">
+					<h1>Dashboard My Project</h1>
+					<p>One kind Of Musical Instrument Shop</p>
+				</div>
+                <div class="floatright">
+                    <div class="floatleft">
+                        <img src="img/touhid.jpg" alt="Profile Pic" />
+                    </div>
+                    <?php
+                    if(isset($_GET['action']) && $_GET['action'] == "logout"){
+                        Session::destroy();
+                    }
+                    ?>
+                    
+                    <div class="floatleft marginleft10">
+                        <ul class="inline-ul floatleft">
+                            <li>Hello <?php echo Session::get('adminName'); ?></li>
+                            <li><a href="?action=logout">Logout</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="clear">
+                </div>
+            </div>
+        </div>
+        <div class="clear">
+        </div>
+        <div class="grid_12">
+            <ul class="nav main">
+                <li class="ic-dashboard"><a href="dashboard.php"><span>Dashboard</span></a> </li>
+                <li class="ic-form-style"><a href=""><span>User Profile</span></a></li>
+				<li class="ic-typography"><a href="changepassword.php"><span>Change Password</span></a></li>
+				<li class="ic-grid-tables"><a href="inbox.php"><span>Inbox</span></a></li>
+                <li class="ic-charts"><a href=""><span>Visit Website</span></a></li>
+            </ul>
+        </div>
+        <div class="clear">
+        </div>
+    
